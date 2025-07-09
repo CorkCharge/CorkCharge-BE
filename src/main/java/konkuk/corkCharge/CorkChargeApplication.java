7386f251d8621a0bf23c4915246cb3f1650259e6
Jay Park <pjh6183@naver.com>
2025-07-09 16:07:47 +0900

    Initial commit

---
+++ src/main/java/konkuk/corkCharge/CorkChargeApplication.java
@@ -0,0 +1,13 @@
+package konkuk.corkCharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorkChargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorkChargeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konkuk/corkCharge/CorkChargeApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.corkCharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorkChargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
