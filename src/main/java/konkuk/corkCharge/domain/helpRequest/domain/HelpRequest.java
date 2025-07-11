6d93cb94012bae77dd42e9442e213131923494ab
Jay Park <pjh6183@naver.com>
2025-07-11 22:05:07 +0900

    [feat]: HelpRequest 엔티티 개발

---
+++ src/main/java/konkuk/corkCharge/domain/helpRequest/domain/HelpRequest.java
@@ -0,0 +1,31 @@
+package konkuk.corkCharge.domain.helpRequest.domain;
+
+import jakarta.persistence.*;
+import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
+import konkuk.corkCharge.domain.user.domain.User;
+import konkuk.corkCharge.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "help_request")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HelpRequest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "help_id")
+    private Long helpId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+}
