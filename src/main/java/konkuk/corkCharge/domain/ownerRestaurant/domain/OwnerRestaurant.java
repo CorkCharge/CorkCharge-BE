54d639c701a27ace9d5b9a3a9c2367fe4d9feb30
Jay Park <pjh6183@naver.com>
2025-07-11 22:05:37 +0900

    [feat]: OwnerRestaurant 엔티티 개발

---
+++ src/main/java/konkuk/corkCharge/domain/ownerRestaurant/domain/OwnerRestaurant.java
@@ -0,0 +1,28 @@
+package konkuk.corkCharge.domain.ownerRestaurant.domain;
+
+import jakarta.persistence.*;
+import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
+import konkuk.corkCharge.domain.user.domain.User;
+import konkuk.corkCharge.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "owner_restaurant")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OwnerRestaurant extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "corkage_submit_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
