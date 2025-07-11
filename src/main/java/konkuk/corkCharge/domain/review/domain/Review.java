49c789338f6098cd08544967e1e800f83ee0a26a
Jay Park <pjh6183@naver.com>
2025-07-11 22:02:36 +0900

    [feat]: Review 엔티티 개발

---
+++ src/main/java/konkuk/corkCharge/domain/review/domain/Review.java
@@ -0,0 +1,34 @@
+package konkuk.corkCharge.domain.review.domain;
+
+import jakarta.persistence.*;
+import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
+import konkuk.corkCharge.domain.user.domain.User;
+import konkuk.corkCharge.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "review")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private Integer rating;
+}
