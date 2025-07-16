b25d17bc8d92ebd5ec9abbcbf654d17e61562cb5
Jay Park <pjh6183@naver.com>
2025-07-16 23:07:16 +0900

    [feat]: 예외처리 코드 추가

--- src/main/java/konkuk/corkCharge/domain/restaurant/service/RestaurantService.java
+++ src/main/java/konkuk/corkCharge/domain/restaurant/service/RestaurantService.java
@@ -3,11 +3,14 @@ package konkuk.corkCharge.domain.restaurant.service;
 import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantListResponse;
 import konkuk.corkCharge.domain.restaurant.repository.RestaurantRepository;
+import konkuk.corkCharge.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static konkuk.corkCharge.global.response.status.BaseExceptionResponseStatus.RESTAURANT_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,10 @@ public class RestaurantService {
     @Transactional(readOnly = true)
     public List<GetRestaurantListResponse> getCorkageRestaurants() {
         List<Restaurant> restaurants = restaurantRepository.findByHasCorkageTrue();
+
+        if (restaurants.isEmpty()) {
+            throw new CustomException(RESTAURANT_NOT_FOUND);
+        }
 
         return restaurants.stream()
                 .map(GetRestaurantListResponse::from)
