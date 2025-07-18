7700a577e85131d0ff7d7a97c8453bdae07d90f1
changyu13 <bigstar002@naver.com>
2025-07-18 15:16:38 +0900

    [chore] : BaseExceptionResponseStatus 에러 항목 추가

--- src/main/java/konkuk/corkCharge/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/konkuk/corkCharge/global/response/status/BaseExceptionResponseStatus.java
@@ -9,6 +9,7 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     BAD_REQUEST(40000, "유효하지 않은 요청입니다."),
     NOT_FOUND(40400, "존재하지 않는 API입니다."),
     INTERNAL_SERVER_ERROR(50000, "서버 내부 오류입니다."),
+    USER_NOT_FOUND(40401, "해당 사용자를 찾을 수 없습니다.")
     ;
 
     private final boolean success = false;
