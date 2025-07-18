7a5c5b02efd6d2ac7126948c069a400e529c8f63
changyu13 <bigstar002@naver.com>
2025-07-18 19:26:41 +0900

    [chore] : ImageType USER 타입 추가

--- src/main/java/konkuk/corkCharge/domain/image/domain/ImageType.java
+++ src/main/java/konkuk/corkCharge/domain/image/domain/ImageType.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ImageType {
     MAIN("가게 사진"),
-    MENU("메뉴 사진")
+    MENU("메뉴 사진"),
+    USER("유저 사진")
     ;
 
     private final String imageType;
