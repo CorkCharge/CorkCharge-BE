0e0549fdf9ed10954ebcf255c6d9c3cbb13dee84
changyu13 <bigstar002@naver.com>
2025-07-18 19:28:17 +0900

    [feat] : UserService 프로필 수정 기능 구현

--- src/main/java/konkuk/corkCharge/domain/user/service/UserService.java
+++ src/main/java/konkuk/corkCharge/domain/user/service/UserService.java
@@ -3,13 +3,16 @@ package konkuk.corkCharge.domain.user.service;
 import konkuk.corkCharge.domain.image.domain.Image;
 import konkuk.corkCharge.domain.image.repository.ImageRepository;
 import konkuk.corkCharge.domain.user.domain.User;
+import konkuk.corkCharge.domain.user.dto.request.PutUserProfileRequest;
 import konkuk.corkCharge.domain.user.dto.response.GetUserProfileResponse;
 import konkuk.corkCharge.domain.user.repository.UserRepository;
 import konkuk.corkCharge.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import static konkuk.corkCharge.domain.image.domain.ImageType.USER;
 import static konkuk.corkCharge.global.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
 
 @Service
@@ -34,5 +37,32 @@ public class UserService {
                 imageUrl
         );
     }
+
+    @Transactional
+    public void updateUserProfile(Long userId, String name, MultipartFile imageFile){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        if(name != null){
+            user.setName(name);
+        }
+
+        if(imageFile != null && !imageFile.isEmpty()){
+            String newImageUrl = s3Uploader.upload(imageFile);
+
+            imageRepository.findProfileImageByUser_UserId(userId)
+                    .ifPresentOrElse(
+                            existingImage-> {
+                                s3Uploader.delete(existingImage.getImageUrl());
+
+                                existingImage.setImageUrl(newImageUrl);
+                            },
+                            () -> {
+                                Image newImage = new Image(user, newImageUrl, USER);
+                            }
+                            );
+        }
+    }
+
 
 }
