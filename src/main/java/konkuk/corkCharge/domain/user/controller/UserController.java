47eed48cc3760622f3fc0e5994b4fbc3d40f7c90
changyu13 <bigstar002@naver.com>
2025-07-18 19:27:53 +0900

    [feat] : UserController 프로필 수정 api 구현

--- src/main/java/konkuk/corkCharge/domain/user/controller/UserController.java
+++ src/main/java/konkuk/corkCharge/domain/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package konkuk.corkCharge.domain.user.controller;
 
+import konkuk.corkCharge.domain.user.dto.request.PutUserProfileRequest;
 import konkuk.corkCharge.domain.user.dto.response.GetUserProfileResponse;
 import konkuk.corkCharge.domain.user.service.UserService;
 import konkuk.corkCharge.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/users")
@@ -16,5 +18,11 @@ public class UserController {
     @GetMapping
     public BaseResponse<GetUserProfileResponse> getUserProfile(@RequestParam Long userId){
         return BaseResponse.ok(userService.getUserProfile(userId));
+    }
+
+    @PutMapping("/modify")
+    public BaseResponse<Void> editUserProfile(@RequestParam Long userId, @RequestPart(required = false) PutUserProfileRequest request, @RequestPart(required = false) MultipartFile image){
+        userService.updateUserProfile(userId, request.name(), image);
+        return BaseResponse.ok();
     }
 }
