5d897b1460a3e2aa7186e0b93a98d53af4c9d02f
changyu13 <bigstar002@naver.com>
2025-07-18 15:13:20 +0900

    [feat]: UserProfileResponseDto 생성

---
+++ src/main/java/konkuk/corkCharge/domain/user/dto/response/UserProfileResponseDto.java
@@ -0,0 +1,12 @@
+package konkuk.corkCharge.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserProfileResponseDto {
+    private String name;
+    private String social_id;
+    private String image_url;
+}
