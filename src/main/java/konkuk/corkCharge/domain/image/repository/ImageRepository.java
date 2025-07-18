0dcb9b03291a03ecdd3f7f481d72b96c8efaa390
changyu13 <bigstar002@naver.com>
2025-07-18 15:16:06 +0900

    [feat]: ImageRepository 생성

---
+++ src/main/java/konkuk/corkCharge/domain/image/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package konkuk.corkCharge.domain.image.repository;
+
+import konkuk.corkCharge.domain.image.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findFirstByUserId(Long userId);
+}
