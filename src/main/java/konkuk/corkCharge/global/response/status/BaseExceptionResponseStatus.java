7cd7b1c5763df3a638990a51ff36249a97eb5b86
Jay Park <pjh6183@naver.com>
2025-07-19 03:16:09 +0900

    [feat]: S3 이미지 업로드 로직 구현

--- src/main/java/konkuk/corkCharge/global/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/konkuk/corkCharge/global/response/status/BaseExceptionResponseStatus.java
@@ -24,9 +24,12 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     /**
      * 70000 : User 도메인 에러 코드
      */
+    USER_NOT_FOUND(70001, "해당 사용자를 찾을 수 없습니다."),
 
-
-    USER_NOT_FOUND(70001, "해당 사용자를 찾을 수 없습니다.")
+    /**
+     * 80000 : Image
+     */
+    FAILED_UPLOAD(80000, "S3 업로드를 실패했습니다.")
     ;
 
     private final boolean success = false;
