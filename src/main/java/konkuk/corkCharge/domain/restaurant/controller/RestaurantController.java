6135e2b88a08ea5520f3d90f17d055620773821e
Jay Park <pjh6183@naver.com>
2025-07-20 22:59:31 +0900

    [feat]: 매장 상세페이지 조회 controller 구현

--- src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
@@ -1,11 +1,13 @@
 package konkuk.corkCharge.domain.restaurant.controller;
 
+import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantDetailResponse;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantListResponse;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantMapResponse;
 import konkuk.corkCharge.domain.restaurant.service.RestaurantService;
 import konkuk.corkCharge.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +29,12 @@ public class RestaurantController {
     public BaseResponse<List<GetRestaurantMapResponse>> getCorkageMap() {
         return BaseResponse.ok(restaurantService.getRestaurantMap());
     }
+
+    @GetMapping("/{restaurantId}")
+    public BaseResponse<GetRestaurantDetailResponse> getRestaurantDetail(
+            @PathVariable(name = "restaurantId") Long restaurantId
+    ){
+        return BaseResponse.ok(restaurantService.getRestaurantDetail(restaurantId));
+    }
+
 }
