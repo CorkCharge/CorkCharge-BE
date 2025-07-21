cf196ac0c713d3352c34a9c97632212b2abaa7af
changyu13 <bigstar002@naver.com>
2025-07-21 18:42:41 +0900

    [chore] : PostSuggestionRequest 필드 수정

--- src/main/java/konkuk/corkCharge/domain/suggestion/dto/request/PostSuggestionRequest.java
+++ src/main/java/konkuk/corkCharge/domain/suggestion/dto/request/PostSuggestionRequest.java
@@ -1,9 +1,11 @@
 package konkuk.corkCharge.domain.suggestion.dto.request;
 
+import konkuk.corkCharge.domain.suggestion.domain.SuggestionCategory;
+
 public record PostSuggestionRequest (
         Long restaurantId,
         String content,
-        String category
+        SuggestionCategory category
 ){
 
 }
