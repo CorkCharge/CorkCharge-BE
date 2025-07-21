fc646a03ab3092d1e73833f7e8cc63b31caa868a
Jay Park <pjh6183@naver.com>
2025-07-21 18:07:30 +0900

    [feat]: 매장 검색 controller 구현

--- src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
@@ -3,13 +3,11 @@ package konkuk.corkCharge.domain.restaurant.controller;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantDetailResponse;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantListResponse;
 import konkuk.corkCharge.domain.restaurant.dto.response.GetRestaurantMapResponse;
+import konkuk.corkCharge.domain.restaurant.dto.response.GetSearchRestaurantResponse;
 import konkuk.corkCharge.domain.restaurant.service.RestaurantService;
 import konkuk.corkCharge.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,8 +31,15 @@ public class RestaurantController {
     @GetMapping("/{restaurantId}")
     public BaseResponse<GetRestaurantDetailResponse> getRestaurantDetail(
             @PathVariable(name = "restaurantId") Long restaurantId
-    ){
+    ) {
         return BaseResponse.ok(restaurantService.getRestaurantDetail(restaurantId));
+    }
+
+    @GetMapping("/search")
+    public BaseResponse<List<GetSearchRestaurantResponse>> searchRestaurant(
+            @RequestParam(name = "keyword") String keyword
+    ) {
+        return BaseResponse.ok(restaurantService.searchRestaurants(keyword));
     }
 
 }
