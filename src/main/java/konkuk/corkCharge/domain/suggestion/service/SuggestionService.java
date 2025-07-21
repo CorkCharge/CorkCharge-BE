10996bd9613ca92e67a8928a55d57408ce1cd017
changyu13 <bigstar002@naver.com>
2025-07-21 18:45:34 +0900

    [feat] : SuggestionService - createSuggestion 함수 구현

--- src/main/java/konkuk/corkCharge/domain/suggestion/service/SuggestionService.java
+++ src/main/java/konkuk/corkCharge/domain/suggestion/service/SuggestionService.java
@@ -1,19 +1,17 @@
 package konkuk.corkCharge.domain.suggestion.service;
 
-import konkuk.corkCharge.domain.image.domain.Image;
-import konkuk.corkCharge.domain.image.repository.ImageRepository;
 import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
+import konkuk.corkCharge.domain.restaurant.repository.RestaurantRepository;
+import konkuk.corkCharge.domain.suggestion.domain.Suggestion;
 import konkuk.corkCharge.domain.suggestion.dto.request.PostSuggestionRequest;
 import konkuk.corkCharge.domain.suggestion.repository.SuggestionRepository;
 import konkuk.corkCharge.domain.user.domain.User;
-import konkuk.corkCharge.domain.user.dto.response.GetUserProfileResponse;
 import konkuk.corkCharge.domain.user.repository.UserRepository;
-import konkuk.corkCharge.domain.user.service.UserService;
 import konkuk.corkCharge.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import static konkuk.corkCharge.global.response.status.BaseExceptionResponseStatus.RESTAURANT_NOT_FOUND;
 import static konkuk.corkCharge.global.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
 
 @Service
@@ -21,13 +19,24 @@ import static konkuk.corkCharge.global.response.status.BaseExceptionResponseStat
 public class SuggestionService {
     private final SuggestionRepository suggestionRepository;
     private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @Transactional
     public void createSuggestion(Long userId, PostSuggestionRequest request){
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
-        Restaurant restaurant = restaurantRepository.findById()
+        Restaurant restaurant = restaurantRepository.findById(request.restaurantId())
+                .orElseThrow(() -> new CustomException(RESTAURANT_NOT_FOUND));
+
+        Suggestion suggestion = new Suggestion(
+                user,
+                restaurant,
+                request.content(),
+                request.category()
+        );
+
+        suggestionRepository.save(suggestion);
     }
 
 }
