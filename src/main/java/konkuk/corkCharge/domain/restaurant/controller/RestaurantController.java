603f2a522be693427b518e2c2f3675d98e45f645
Jay Park <pjh6183@naver.com>
2025-07-23 02:16:40 +0900

    [feat]: 지역 필터링 controller 구현

--- src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/konkuk/corkCharge/domain/restaurant/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package konkuk.corkCharge.domain.restaurant.controller;
 
+import konkuk.corkCharge.domain.restaurant.dto.request.GetFilterRequest;
 import konkuk.corkCharge.domain.restaurant.dto.response.*;
 import konkuk.corkCharge.domain.restaurant.service.RestaurantService;
 import konkuk.corkCharge.global.response.BaseResponse;
@@ -42,6 +43,13 @@ public class RestaurantController {
     @GetMapping("/hot")
     public BaseResponse<List<GetHotRestaurantResponse>> getHotRestaurant() {
         return BaseResponse.ok(restaurantService.getHotRestaurants());
+    }
+
+    @GetMapping("/filter")
+    public BaseResponse<List<?>> filterRestaurant(
+            @ModelAttribute GetFilterRequest request
+    ) {
+        return BaseResponse.ok(restaurantService.filterRestaurants(request));
     }
 
 }
