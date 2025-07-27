b686d0c4f884fd7a3948c5fa96547957940ce658
changyu13 <bigstar002@naver.com>
2025-07-27 22:49:14 +0900

    [feat] : GetReviewResponse 생성

---
+++ src/main/java/konkuk/corkCharge/domain/user/dto/response/GetReviewResponse.java
@@ -0,0 +1,13 @@
+package konkuk.corkCharge.domain.user.dto.response;
+
+import java.time.LocalDateTime;
+
+public record GetReviewResponse(
+        Long reviewId,
+        Long restaurantId,
+        Long userId,
+        String content,
+        int rating,
+        LocalDateTime createdAt
+) {
+}
