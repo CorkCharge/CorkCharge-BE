e96b023d5cb63db343d4ef2334efed3b8ade8118
Jay Park <pjh6183@naver.com>
2025-08-01 01:28:35 +0900

    [feat]: 콜키지 필터링 api repository 구현

--- src/main/java/konkuk/corkCharge/domain/corkageStore/repository/CorkageStoreRepository.java
+++ src/main/java/konkuk/corkCharge/domain/corkageStore/repository/CorkageStoreRepository.java
@@ -2,8 +2,18 @@ package konkuk.corkCharge.domain.corkageStore.repository;
 
 import konkuk.corkCharge.domain.corkageStore.domain.CorkageStore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CorkageStoreRepository extends JpaRepository<CorkageStore, Long> {
+
+    @Query("SELECT cs FROM CorkageStore cs " +
+            "JOIN FETCH cs.restaurant r " +
+            "LEFT JOIN FETCH cs.multiPrices mp " +
+            "LEFT JOIN FETCH cs.corkageOptions co")
+    List<CorkageStore> findAllForRestaurant();
+
 }
