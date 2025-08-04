3341d3ceb50f16ace4e57edf1db0c1942efd735f
changyu13 <bigstar002@naver.com>
2025-08-04 19:25:58 +0900

    [feat]: TipController, TipService getTips 함수 추가

--- src/main/java/konkuk/corkCharge/domain/tip/controller/TipController.java
+++ src/main/java/konkuk/corkCharge/domain/tip/controller/TipController.java
@@ -1,10 +1,10 @@
 package konkuk.corkCharge.domain.tip.controller;
 
 import konkuk.corkCharge.domain.tip.dto.request.PostTipRequest;
+import konkuk.corkCharge.domain.tip.dto.response.GetTipListResponse;
 import konkuk.corkCharge.domain.tip.service.TipService;
 import konkuk.corkCharge.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,6 +23,11 @@ public class TipController {
     ){
         tipService.createTip(request,images);
         return new BaseResponse(null);
+    }
+
+    @GetMapping
+    public BaseResponse<List<GetTipListResponse>> getTips(){
+        return BaseResponse.ok(tipService.getTips());
     }
 
 
