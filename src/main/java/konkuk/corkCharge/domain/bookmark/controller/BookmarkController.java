4edd51d0c605f0c7941a489b996d127020d338fd
changyu13 <bigstar002@naver.com>
2025-08-06 18:03:01 +0900

    [feat]: BookmarkController getSavedRestaurants 함수 추가

--- src/main/java/konkuk/corkCharge/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/konkuk/corkCharge/domain/bookmark/controller/BookmarkController.java
@@ -1,10 +1,14 @@
 package konkuk.corkCharge.domain.bookmark.controller;
 
+import konkuk.corkCharge.domain.bookmark.domain.BookmarkTargetType;
 import konkuk.corkCharge.domain.bookmark.dto.request.PostBookmarkRequest;
+import konkuk.corkCharge.domain.bookmark.dto.response.GetSavedRestaurantResponse;
 import konkuk.corkCharge.domain.bookmark.service.BookmarkService;
 import konkuk.corkCharge.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookmarks")
@@ -13,7 +17,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping
-    public BaseResponse<Void> createBookmark(@RequestParam Long userId, @RequestBody PostBookmarkRequest request){
+    public BaseResponse<Void> createBookmark(@RequestParam Long userId, @RequestBody PostBookmarkRequest request) {
         bookmarkService.createBookmark(userId, request);
         return BaseResponse.ok(null);
     }
@@ -22,5 +26,11 @@ public class BookmarkController {
     public BaseResponse<Void> deleteBookmark(@RequestParam Long userId, @PathVariable Long bookmarkId){
         bookmarkService.deleteBookmark(userId, bookmarkId);
         return BaseResponse.ok(null);
+    }
+
+    @GetMapping("/restaurant")
+    public BaseResponse<List<GetSavedRestaurantResponse>> getSavedRestaurants(
+            @RequestParam Long userId){
+        return BaseResponse.ok(bookmarkService.getSavedRestaurants(userId));
     }
 }
