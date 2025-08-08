77ca815d9284ba094baff8eab90651fc617c98c1
changyu13 <bigstar002@naver.com>
2025-08-08 14:46:08 +0900

    [feat]: OwnerRestaurantRepository findAllByUser_UserIdAndRestaurant_HasCorkageFalse 함수 추가

--- src/main/java/konkuk/corkCharge/domain/ownerRestaurant/repository/OwnerRestaurantRepository.java
+++ src/main/java/konkuk/corkCharge/domain/ownerRestaurant/repository/OwnerRestaurantRepository.java
@@ -5,7 +5,11 @@ import konkuk.corkCharge.domain.restaurant.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OwnerRestaurantRepository extends JpaRepository<OwnerRestaurant, Long> {
     boolean existsByRestaurant(Restaurant restaurant);
+    List<OwnerRestaurant> findAllByUser_UserIdAndRestaurant_HasCorkageFalse(Long userId);
 }
