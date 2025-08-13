365275f794ed4788a5d118226c2361b26168d446
Jay Park <pjh6183@naver.com>
2025-08-13 20:10:49 +0900

    [feat]: 커스텀 어노테이션 구현

---
+++ src/main/java/konkuk/corkCharge/global/annotation/LoginUserId.java
@@ -0,0 +1,8 @@
+package konkuk.corkCharge.global.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LoginUserId {}
