0b288bac706a41d9ee5829e8d0c0f8bb94f7ac60
Jay Park <pjh6183@naver.com>
2025-08-21 01:35:33 +0900

    [chore]: user, OwnerRestaurant 매핑

--- src/main/java/konkuk/corkCharge/domain/user/domain/User.java
+++ src/main/java/konkuk/corkCharge/domain/user/domain/User.java
@@ -2,6 +2,7 @@ package konkuk.corkCharge.domain.user.domain;
 
 import jakarta.persistence.*;
 import konkuk.corkCharge.domain.helpRequest.domain.HelpRequest;
+import konkuk.corkCharge.domain.ownerRestaurant.domain.OwnerRestaurant;
 import konkuk.corkCharge.domain.review.domain.Review;
 import konkuk.corkCharge.global.entity.BaseEntity;
 import lombok.*;
@@ -44,6 +45,9 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<HelpRequest> helpRequests = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<OwnerRestaurant> ownerRestaurants = new ArrayList<>();
 
     public void addReview(Review review) {
         reviews.add(review);
