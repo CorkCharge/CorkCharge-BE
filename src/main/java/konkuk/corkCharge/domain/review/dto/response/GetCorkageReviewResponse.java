e637837dd7ce83867ced37a5c0620c849624f21e
Jay Park <pjh6183@naver.com>
2026-01-11 00:32:04 +0900

    [feat] 콜키지 리뷰 조회 response dto 구현

---
+++ src/main/java/konkuk/corkCharge/domain/review/dto/response/GetCorkageReviewResponse.java
@@ -0,0 +1,17 @@
+package konkuk.corkCharge.domain.review.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GetCorkageReviewResponse(
+        Long reviewId,
+        Long restaurantId,
+        String restaurantName,
+        String writer,
+        String content,
+        int rating,
+        LocalDateTime createdAt,
+        List<String> imageUrls,
+        int bookmarkCount
+) {
+}
