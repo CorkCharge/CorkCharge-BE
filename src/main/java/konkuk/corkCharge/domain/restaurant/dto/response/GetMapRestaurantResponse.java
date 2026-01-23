be783db35f595d0ad8d623791b1b327cfd380d22
Jay Park <pjh6183@naver.com>
2026-01-23 18:56:55 +0900

    [feat] 콜키지맵 조회 request dto 구현

--- src/main/java/konkuk/corkCharge/domain/restaurant/dto/response/GetMapRestaurantResponse.java
+++
@@ -1,9 +0,0 @@
-package konkuk.corkCharge.domain.restaurant.dto.response;
-
-public record GetMapRestaurantResponse(
-        Long restaurantId,
-        Double latitude,
-        Double longitude,
-        String price
-) {
-}
