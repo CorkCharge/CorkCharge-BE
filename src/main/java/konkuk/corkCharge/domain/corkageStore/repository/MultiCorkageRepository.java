66d76a52d549e8d1e89257eac3faf35753e71839
Jay Park <pjh6183@naver.com>
2026-01-26 01:07:50 +0900

    [feat] 콜키지 추가/수정에 필요한 다중 콜키지 삭제 메서드 추가

--- src/main/java/konkuk/corkCharge/domain/corkageStore/repository/MultiCorkageRepository.java
+++ src/main/java/konkuk/corkCharge/domain/corkageStore/repository/MultiCorkageRepository.java
@@ -2,8 +2,15 @@ package konkuk.corkCharge.domain.corkageStore.repository;
 
 import konkuk.corkCharge.domain.corkageStore.domain.MultiCorkage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MultiCorkageRepository extends JpaRepository<MultiCorkage, Long> {
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from MultiCorkage mc where mc.corkageStore.corkageStoreId = :corkageStoreId")
+    void deleteAllByCorkageStoreId(@Param("corkageStoreId") Long corkageStoreId);
 }
