d68da362cec673e132ab3cdad6df5365b8282dc0
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-01-30 15:22:43 +0900

    [FEAT] repository 파일 생성

---
+++ src/main/java/konkuk/corkCharge/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package konkuk.corkCharge.domain.notification.repository;
+
+import konkuk.corkCharge.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByCreatedAtAfterOrderByCreatedAtDesc(LocalDateTime from);
+}
